d349f374f5d46ec9e3c36fb6822527900aa7b141
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2025-12-29 14:38:20 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Hono } from "hono";
+import { env as Env } from "cloudflare:workers";
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const scheduledCron = async (
+  controller: ScheduledController,
+  env: typeof Env,
+  ctx: ExecutionContext
+) => {
+  switch (controller.cron) {
+    case "* * * * *":
+      console.log("Every 1 minute");
+      break;
+    case "*/10 * * * *":
+      console.log("Every 10 minutes");
+      break;
+    case "0 * * * *":
+      console.log("Every hour");
+      break;
+    case "0 12 * * *":
+      console.log("Every day at 12:00");
+      break;
+    case "0 12 * * 0":
+      console.log("Every week on Sunday at 12:00");
+      break;
+    default:
+      console.log("Unknown cron schedule");
+      break;
+  }
+};
+
+export default {
+  fetch: app.fetch,
+  scheduled: scheduledCron,
+};
